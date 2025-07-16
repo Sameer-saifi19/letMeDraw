5370f9978a40362c81e628004c1db73814be0a1d
Sameer <sameer.edu19@gmail.com>
2025-07-16 13:07:44 +0530

    Backend Complete

--- apps/web/hooks/useSocket.ts
+++ apps/web/hooks/useSocket.ts
@@ -6,7 +6,7 @@ export function useSocket() {
     const [ socket, setSocket ] = useState<WebSocket>();
 
     useEffect(() => {
-        const ws = new WebSocket(`${WS_URL}?token=`);
+        const ws = new WebSocket(`${WS_URL}?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJjOGVjYjFlNC0yNGU5LTQ1OWEtYWU5OS1jYmUwNDQyMjE5YzQiLCJpYXQiOjE3NTIzOTYyNjF9.jASWv3_zunWJ-0yIxatzWJxZ271zO4nJpLVmm4wvpXo`);
         ws.onopen = () =>{
             setLoading(false)
             setSocket(ws)
